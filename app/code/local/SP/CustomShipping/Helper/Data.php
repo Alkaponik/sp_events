919719f57a07b6304c9f24d6d25705c1658b5ac9
Pavel Usachev <pusachev@oroinc.com>
2017-01-11 23:17:37 +0200

    - Added new module for custom shipping

---
+++ app/code/local/SP/CustomShipping/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+
+class SP_CustomShipping_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+    public function getExpressMaxWeight()
+    {
+
+    }
+}
--- app/code/local/SP/CustomShipping/Model/Carrier.php
+++ app/code/local/SP/CustomShipping/Model/Carrier.php
@@ -11,6 +11,87 @@ class SP_CustomProduct_Model_Carrier
     protected $_code = 'sp_shipping';
 
     /**
+     * @var SP_CustomShipping_Helper_Data
+     */
+    protected $_helper;
+
+    /**
+     * @var bool
+     */
+    protected $_expressAvailable = true;
+
+    protected function _construct()
+    {
+        $this->_helper = Mage::helper('sp_customshipping');
+    }
+
+    /**
+     * @param Mage_Shipping_Model_Rate_Request $request
+     * @return Mage_Shipping_Model_Rate_Result
+     */
+    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+    {
+        /** @var Mage_Shipping_Model_Rate_Result $result */
+        $result = Mage::getModel('shipping/rate_result');
+
+        $expressMaxWeight = $this->_helper->getExpressMaxWeight();
+
+        foreach ($request->getAllItems() as $item) {
+            if ($item->getWeight > $expressMaxWeight) {
+                $this->_expressAvailable = false;
+                break;
+            }
+        }
+
+        if ($this->_expressAvailable) {
+            $result->append($this->_getExpressRate());
+        }
+        $result->append($this->_getStandardRate());
+
+        return $result;
+    }
+
+    /**
+     * @return Mage_Shipping_Model_Rate_Result_Method
+     */
+    protected function _getStandardRate()
+    {
+        /** @var Mage_Shipping_Model_Rate_Result_Method $rate */
+        $rate = Mage::getModel('shipping/rate_result_method');
+
+        $rate->setCarrier($this->_code);
+        $rate->setCarrierTitle($this->getConfigData('title'));
+        $rate->setMethod('large');
+        /** @TODO get method title from system config */
+        $rate->setMethodTitle('Standart delivery');
+        /** @TODO get price from system config */
+        $rate->setPrice(1.5);
+        $rate->setCost(0);
+
+        return $rate;
+    }
+
+    /**
+     * @return Mage_Shipping_Model_Rate_Result_Method
+     */
+    protected function _getExpressRate()
+    {
+        /** @var Mage_Shipping_Model_Rate_Result_Method $rate */
+        $rate = Mage::getModel('shipping/rate_result_method');
+
+        $rate->setCarrier($this->_code);
+        $rate->setCarrierTitle($this->getConfigData('title'));
+        $rate->setMethod('large');
+        /** @TODO get method title from system config */
+        $rate->setMethodTitle('Express delivery');
+        /** @TODO get price from system config */
+        $rate->setPrice(10);
+        $rate->setCost(0);
+
+        return $rate;
+    }
+
+    /**
      * @return string[]
      */
     public function getAllowedMethods()
