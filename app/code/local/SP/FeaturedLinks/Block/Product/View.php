bba16e5620b9c714d7211beab6506567ca80405e
Pavel Usachev <pusachev@oroinc.com>
2017-03-11 14:47:17 +0200

    - Added new module for custom payment

---
+++ app/code/local/SP/FeaturedLinks/Block/Product/View.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Pavel Usachev <webcodekeeper@hotmail.com>
+ * @copyright Copyright (c) 2017, Pavel Usachev
+ */
+class SP_FeaturedLinks_Block_Product_View
+    extends Mage_Catalog_Block_Product_View
+{
+    public function getProduct()
+    {
+        if (!Mage::registry('product') && $this->getProductId()) {
+            $product = Mage::getModel('catalog/product')->load($this->getProductId());
+            Mage::register('product', $product);
+        }
+        return Mage::registry('product');
+    }
+}
